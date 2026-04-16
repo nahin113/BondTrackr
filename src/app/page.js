2446ee4601b6e4a4b8f74f081fced8405be24ffe
nahin113 <nahinahmed113@gmail.com>
2026-04-16 22:58:27 +0600

    build home banner section with call-to-action and summary statistics cards

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
+import Banner from "@/components/home/Banner";
+import SummaryCards from "@/components/home/SummaryCards";
+
 export default function Home() {
   return (
-    <div>
-      <h1>This is Homepage</h1>
+    <div className="bg-base-300">
+      <Banner></Banner>
+      <SummaryCards></SummaryCards>
+      <div className="py-[40px]">
+      <div className="divider w-8/12 mx-auto"></div>
+      </div>
     </div>
   );
 }
